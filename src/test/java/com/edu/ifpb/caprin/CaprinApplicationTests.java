61951d1466df9076954752cd03c0e43f8648c545
Ellen Cassia Matos <ellencassiamatos@gmail.com>
2024-10-23 14:56:03 -0300

    first commit

---
+++ src/test/java/com/edu/ifpb/caprin/CaprinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ifpb.caprin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaprinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
