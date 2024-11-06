0c43c1c491008ae7e8a958202ac235e202fc951c
Ellen Cassia Matos <ellencassiamatos@gmail.com>
2024-11-05 21:23:02 -0300

    :sparkles: feat: mudanças do repository

--- src/main/java/com/edu/ifpb/caprin/model/repository/animal/AnimalRepository.java
+++ src/main/java/com/edu/ifpb/caprin/model/repository/animal/AnimalRepository.java
@@ -5,7 +5,4 @@ import com.edu.ifpb.caprin.model.compartilhado.repositorio.DominioRepositorio;
 import com.edu.ifpb.caprin.model.entity.animal.Animal;
 
 @Repository
-public interface AnimalRepository extends DominioRepositorio<Animal, Long> {
-public interface AnimalRepository extends DominioRepositorio<Animal, Long> {
-    
-}
+public interface AnimalRepository extends DominioRepositorio<Animal, Long> {}
